3c8ef197bdd5af2e064f876bbf155400ae618de8
Marius <brazasmarius@gmail.com>
2026-05-03 19:52:50 +0000

    script: update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "Updating system..."
+echo "----- Updating system -----"
 sudo apt update && sudo apt upgrade -y
 
-echo "Installing nginx..."
+echo "----- Installing openSSH -----"
+sudo apt install -y openssh-server
+
+echo "----- Installing git -----"
+sudo apt install -y git
+
+echo "----- Installing nginx -----"
 sudo apt install nginx -y
 
-echo "All done!"
+echo "----- All done! -----"
